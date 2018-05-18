069befd54600894eddcdffbf0b3c46eae481ed01
Taras B <Taras.Buha@gmail.com>
2018-05-18 13:43:04 +0300

    Add project files.

---
+++ IntegrationSamples/Controllers/FBBotController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using IntegrationSamples.Models;
+
+namespace IntegrationSamples.Controllers
+{
+    public class FBBotController : Controller
+    {
+        // GET: FBBot
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Receive()
+        {
+            var query = Request.QueryString;
+
+            //_logWriter.WriteLine(Request.RawUrl);
+
+            if (query["hub.mode"] == "subscribe" &&
+                query["hub.verify_token"] == AppSettings.verify_token)
+            {
+                //string type = Request.QueryString["type"];
+                var retVal = query["hub.challenge"];
+                return Json(int.Parse(retVal), JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return HttpNotFound("Receive Failed");
+            }
+        }
+
+        [ActionName("Receive")]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult ReceivePost(BotRequest data)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                foreach (var entry in data.entry)
+                {
+                    foreach (var message in entry.messaging)
+                    {
+                        if (string.IsNullOrWhiteSpace(message?.message?.text))
+                            continue;
+
+                        var msg = "You said: " + message.message.text;
+                        var json = $@" {{recipient: {{  id: {message.sender.id}}},message: {{text: ""{msg}"" }}}}";
+                        PostRaw($"https://graph.facebook.com/v2.6/me/messages?access_token={AppSettings.access_token}", json);
+                    }
+                }
+            });
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+        private string PostRaw(string url, string data)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            using (var requestWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                requestWriter.Write(data);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            if (response == null)
+                throw new InvalidOperationException("GetResponse returns null");
+
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
